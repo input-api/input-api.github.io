1c6dde9a48023f71f39d6d39d523c5763fc2f1b7
input-api <inputapi@gmail.com>
2024-04-25 22:26:58 +0300

    add bg image v11

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ menuItems.forEach(function(menuItem) {
         menuItem.classList.toggle('details');
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+  
+    var backgroundImageUrl = "source/img/bg_forest.jpg"; 
+    var backgroundSection = document.querySelector('#menu-body');
+    backgroundSection.style.backgroundImage = "url('" + backgroundImageUrl + "')";
+    backgroundSection.style.width = windowWidth + 'px';
+    backgroundSection.style.height = windowHeight + 'px';
+    backgroundSection.style.backgroundSize = "cover";
+    backgroundSection.style.backgroundRepeat = "no-repeat"; 
+    backgroundSection.style.backgroundAttachment = "fixed"; 
+  });
+  
