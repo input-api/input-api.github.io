393967ebe71a6408558f11c18401ed21421e29a3
input-api <inputapi@gmail.com>
2024-05-04 22:28:50 +0300

    setup seasonal menu

--- js/main.js
+++ js/main.js
@@ -9,12 +9,22 @@ menuItems.forEach(function(menuItem) {
 const mainManuBlock = document.querySelector('#main-menu');
 const seasonalManuBlock = document.querySelector('#seasonal-menu');
 const controlButton = document.querySelector('#change-menu');
+const bg = document.querySelector('#bg_image');
+
 
 controlButton.addEventListener('click', function(){
     if (seasonalManuBlock.classList.contains('hidden')){
         seasonalManuBlock.classList.remove('hidden');
+        controlButton.textContent = "Return to main menu";
+        bg.style.backgroundImage = "linear-gradient(to bottom, #808080, #b46f08)";
     } else {
         seasonalManuBlock.classList.add('hidden');
+        controlButton.textContent = "Open seasonal menu";
+        bg.style.backgroundImage = "linear-gradient(to bottom, #808080, #013220)";
     }
     mainManuBlock.classList.toggle('hidden');
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' 
+    });
 });
